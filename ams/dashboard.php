56f72699f2bdc221fe352ef9ee9b96c9e6746ecf
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-02 11:49:34 +0100

    LOGIN HERE

--- ams/dashboard.php
+++ ams/dashboard.php
@@ -114,7 +114,7 @@ include("header.php");
                     <div class="col-md-6 col-lg-3">
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title text-uppercase">Tera Ad Payment(Reps & Managers)</h5>
+                                <h5 class="card-title text-uppercase">Tera Ad Spent</h5>
                                 <div class="d-flex align-items-center mb-2 mt-4">
                                     <h2 class="mb-0 display-5"><i class="ti-wallet text-dark"></i></h2>
                                     <div class="ml-auto">
